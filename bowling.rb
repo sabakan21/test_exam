31dab39f9599bdb156f1154d2b83c7f35b5be6e4
FUJII Akira <fujii@sein.mse.kyutech.ac.jp>
2015-06-08 17:40:32 +0900

    fix to test

--- bowling.rb
+++ bowling.rb
@@ -1,15 +1,22 @@
 require 'minitest/autorun'
+require 'minitest/unit'
 
+class Bowl 
 
-def score_calc(input)
-  # == write here! ==
+  def score(input)
+    # == write here! ==
+    true
+  end
 end
 
-class TestBowling < MiniTest::Test
 
+class TestBowling < MiniTest::Test
+  def setup
+    @bowl = Bowl.new
+  end
   def test_final
     input = [1,4,4,5,6,4,5,5, 10, 0,1,7,3,6,4,10,2,8,6]
-    out = score_calc(input)
+    out = @bowl.score(input)
     assert_equal(133, out)
   end
 end
