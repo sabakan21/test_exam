00d41f9690bee0090310f71249dd0dd5e6d3e2d2
sabakan21 <fujii@sein.mse.kyutech.ac.jp>
2015-06-08 11:40:58 +0900

    add test_1

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,4 +11,8 @@ class TestFizzBuzz < MiniTest::Test
   def test_have_method?
     assert Integer.method_defined?(:fizzbuzz), "Integer dose not have fizzbuzz method"
   end
+
+  def test_1
+    assert_equal(1, 1.fizzbuzz, "should return num")
+  end
 end
